18dcfbcd15a047394ed1adad6ad44c0df29b70b4
Min Liu <minliu905@gmail.com>
2024-07-09 12:27:48 -0700

    Update token interface

--- include/jqcpp/json_tokenizer.hpp
+++ include/jqcpp/json_tokenizer.hpp
@@ -1,7 +1,29 @@
 #pragma once
+#include <stdexcept>
 #include <string>
 #include <vector>
 namespace jqcpp {
+
+enum class TokenType {
+  LeftBrace,
+  RightBrace,
+  LeftBracket,
+  RightBracket,
+  Comma,
+  Colon,
+  Strinig,
+  Number,
+  True,
+  False,
+  Null
+};
+
+struct Token {
+  TokenType type;
+  std::string value;
+  Token(TokenType t, const std::string &v = "") : type(t), value(v) {}
+};
+
 class JSONTokenizer {
 public:
   std::vector<std::string> tokenize(const std::string &json_string);
@@ -9,5 +31,13 @@ public:
 private:
   bool is_whilespace(char c);
   bool is_digit(char c);
+  std::string parse_string(const std::string &json_string, size_t &i);
+  std::string parse_number(const std::string &json_string, size_t &i);
+};
+
+class JSONTokenizerError : public std::runtime_error {
+public:
+  JSONTokenizerError(const std::string &message)
+      : std::runtime_error(message) {}
 };
 } // namespace jqcpp
